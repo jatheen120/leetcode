91ea8fee6c2b017dc66173cbe40ad248ff694b14
Jatheen_K <jatheen120@gmail.com>
2026-03-25 15:58:10 +0530

    Time: 4 ms (97.20%) | Memory: 160.4 MB (89.51%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
@@ -1,30 +1,47 @@
 class Solution {
     public boolean canPartitionGrid(int[][] grid) {
-        int rows=grid.length;
-        int cols=grid[0].length;
+        int n=grid.length,m=grid[0].length;
+        long hor[]= new long[n];
+        long total1=0;
+        for(int i=0;i<n;i++)
+        {
+            long temp=0;
+            for(int j=0;j<m;j++)
+            {
+                int t=grid[i][j];
+                 temp+=t;
+                 
+            }
+            hor[i]=temp+total1;
+            total1+=temp;
+        }
+       
+        for(int i=0;i<n;i++)
+        {
+            if(hor[i]==total1-hor[i])
+            return true;
+        }
 
-        long total=0;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                total+=grid[i][j];
+        long ver[]= new long[m];
+        long total2=0;
+        for(int j=0;j<m;j++)
+        {
+            long temp=0;
+            for(int i=0;i<n;i++)
+            {
+                temp+=grid[i][j];
             }
+            ver[j]=temp+total2;
+            total2+=temp;
         }
-        long sum=0;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                sum+=grid[i][j];
-            }
-            if(sum*2==total) return true;
-        }
-        
-        sum=0;
-        for(int j=0;j<cols-1;j++){
-            for(int i=0;i<rows;i++){
-                sum+=grid[i][j];
-            }
-            if(sum*2==total) return true;
+
+        for(int i=0;i<m;i++)
+        {
+            if(ver[i]==total2-ver[i])
+            return true;
         }
 
         return false;
+
     }
 }
