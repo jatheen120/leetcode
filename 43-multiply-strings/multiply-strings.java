518df2774845e1987aa9ee5f4a5f33751e287753
Jatheen_K <jatheen120@gmail.com>
2026-03-29 17:16:10 +0530

    Time: 10 ms (8.81%) | Memory: 44.5 MB (19.44%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,8 @@
+import java.math.BigInteger;
+class Solution {
+    public String multiply(String num1, String num2) {
+        return new BigInteger(num1)
+                .multiply(new BigInteger(num2))
+                .toString();
+    }
+}
