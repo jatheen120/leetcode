d6ff16f2a35ca8f712412c725cb55fdf888f5155
Jatheen_K <jatheen120@gmail.com>
2026-04-07 19:15:15 +0530

    Time: 145 ms (5.59%) | Memory: 57.7 MB (80.42%) - LeetSync

--- 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
@@ -1,71 +1,70 @@
-class Robot {
+class Robot{
     int x=0,y=0;
     int width,height;
-    String dir="East";
+    String d="East";
     int per;
-    boolean moved = false;
+    boolean m=false;
 
-    public Robot(int width, int height) {
+    public Robot(int width,int height){
         this.width=width;
         this.height=height;
         per=2*(width+height)-4;
     }
 
-    public void step(int num) {
+    public void step(int num){
         num=num%per;
-        if(num==0&(x!=0 || y!=0)) num=per;
-        while (num>0) {
-            if (dir.equals("East")) {
-                if (x+1<width) {
+        if(num==0&(x!=0||y!=0))num=per;
+
+        while(num>0){
+            if(d.equals("East")){
+                if(x+1<width){
                     x++;
                     num--;
-                } else {
-                    dir="North";
-                }
-            }
-            else if (dir.equals("North")) {
-                if (y+1<height) {
-                    y++;
-                    num--;
-                } else {
-                    dir="West";
-                }
-            }
-            else if (dir.equals("West")) {
-                if (x-1>=0) {
-                    x--;
-                    num--;
-                } else {
-                    dir="South";
+                }else{
+                    d="North";
                 }
             }
 
-            else {
-                if (y - 1 >= 0) {
+            else if(d.equals("North")){
+                if(y+1<height){
+                    y++;
+                    num--;
+                }else{
+                    d="West";
+                }
+            }
+
+            else if(d.equals("West")){
+                if(x-1>=0){
+                    x--;
+                    num--;
+                }else{
+                    d="South";
+                }
+            }
+
+            else{
+                if(y-1>=0){
                     y--;
                     num--;
                 }else{
-                    dir="East";
+                    d="East";
                 }
             }
         }
-        moved=true;
-        if (x == 0 && y == 0 && moved) {
-            dir = "South";
+
+        m=true;
+
+        if(x==0&&y==0&&m){
+            d="South";
         }
     }
-    public int[] getPos() {
-        return new int[]{x, y};
+
+    public int[] getPos(){
+        return new int[]{x,y};
     }
-    public String getDir() {
-        return dir;
+
+    public String getDir(){
+        return d;
     }
 }
-
-/**
- * Your Robot object will be instantiated and called as such:
- * Robot obj = new Robot(width, height);
- * obj.step(num);
- * int[] param_2 = obj.getPos();
- * String param_3 = obj.getDir();
- */
