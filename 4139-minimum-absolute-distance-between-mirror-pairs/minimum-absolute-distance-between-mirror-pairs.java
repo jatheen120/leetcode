9804d8848cc3529c71505d0b64c2b5c951f7ad30
Jatheen_K <jatheen120@gmail.com>
2026-04-17 14:57:57 +0530

    Time: 56 ms (37.06%) | Memory: 95 MB (76.65%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.java
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.java
@@ -2,24 +2,19 @@ class Solution {
     public int minMirrorPairDistance(int[] nums) {
         HashMap<Integer,Integer> map=new HashMap<>();
         int min=Integer.MAX_VALUE;
-
         for(int i=0;i<nums.length;i++){
             int rev=r(nums[i]);
-
             if(map.containsKey(nums[i])){
                 min=Math.min(min,i-map.get(nums[i]));
             }
-
             map.put(rev,i);
         }
-
         if(min==Integer.MAX_VALUE){
             return -1;
         }else{
             return min;
         }
     }
-
     private int r(int x){
         int rev=0;
         while(x>0){
